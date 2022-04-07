b6b6420155bca42223183e2c2edb5cbfacfc77c6
Brad Lindsay <bslindsa@gmail.com>
2022-04-06 20:48:06 -0700

    Make triangle points and clickable dots

--- script.js
+++ script.js
@@ -3,20 +3,49 @@ $(document).ready(function () {
         mouseX = ev.pageX;
         mouseY = ev.pageY
         console.log(mouseX + ' ' + mouseY);
-        var color = '#000000';
-        var size = '1px';
         $("body").append(
-        $('<div></div>')
-            .css('position', 'absolute')
-            .css('top', mouseY + 'px')
-            .css('left', mouseX + 'px')
-            .css('width', size)
-            .css('height', size)
-            .css('background-color', color));
+            $('<div></div>')
+                .css('position', 'absolute')
+                .css('top', mouseY + 'px')
+                .css('left', mouseX + 'px')
+                .css('width', '1px')
+                .css('height', '1px')
+                .css('background-color', 'white'));
     });
 });
 
+// Mark triangle points
+let width = $(window).width();
+let height = $(window).height();
 
+$("body").append(
+    $('<div></div>')
+        .css('position', 'absolute')
+        .css('top', '5px')
+        .css('left', width / 2 + 'px')
+        .css('width', '5px')
+        .css('height', '5px')
+        .css('background-color', 'white'));
+
+$("body").append(
+    $('<div></div>')
+        .css('position', 'absolute')
+        .css('top', height - 10 + 'px')
+        .css('left', (height + (width / 2)) - 10 + 'px')
+        .css('width', '5px')
+        .css('height', '5px')
+        .css('background-color', 'white'));
+
+$("body").append(
+    $('<div></div>')
+        .css('position', 'absolute')
+        .css('top', height - 10 + 'px')
+        .css('left', (height + (width / 2)) + 100) + 'px')
+    .css('width', '5px')
+    .css('height', '5px')
+    .css('background-color', 'white')
+
+    
 // Pick a random point on the triangle
 const choosePoint = () => {
     return Math.floor(Math.random() * 3)
