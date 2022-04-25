776edb10bfbf94977b296eaa0fb87541a6e1e238
Brad Lindsay <bslindsa@gmail.com>
2022-04-25 14:24:23 -0700

    Limit traingle to isolate box

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
 // Mark triangle points
-const width = $(window).width();
-const height = $(window).height();
+const width = $('#triangle-box').width();
+const height = $('#triangle-box').height();
 const botLeft = { x: (width / 2) + ((height - 40) / 2), y: height - 40 };
 const botRight = { x: ((width / 2)) - ((height - 40) / 2), y: height - 40 };
 const topVert = { x: width / 2, y: 20 };
@@ -12,7 +12,7 @@ let nextPoint = {};
 let points = 0;
 
 // Top Triangle point
-$("body").append(
+$("#triangle-box").append(
     $('<div></div>')
         .css('position', 'absolute')
         .css('top', '20px')
@@ -21,7 +21,7 @@ $("body").append(
         .css('height', '5px')
         .css('background-color', 'white'));
 // Bottom right triangle point
-$("body").append(
+$("#triangle-box").append(
     $('<div></div>')
         .css('position', 'absolute')
         .css('top', botRight.y + 'px')
@@ -30,7 +30,7 @@ $("body").append(
         .css('height', '5px')
         .css('background-color', 'white'));
 // Bottom left triangle point
-$("body").append(
+$("#triangle-box").append(
     $('<div></div>')
         .css('position', 'absolute')
         .css('top', botLeft.y + 'px')
@@ -45,14 +45,14 @@ const calcNextPoint = () => {
     // Pick a random point on the triangle
     let vertex = Math.floor(Math.random() * 3);
     let size = Math.floor(Math.random() * 3);
-    let colorNum = Math.floor(Math.random() * 3);
-    let color = 'red';
+    let colorNum = Math.floor(Math.random() * 2);
+    let color = 'gold';
     if (colorNum === 0) {
         color = 'white';
     } 
-    else if (colorNum === 1) {
-        color = 'blue';
-    }
+    // else if (colorNum === 1) {
+    //     color = 'blue';
+    // }
     
     let point2 = topVert;
     if (vertex === 0) {
@@ -68,7 +68,7 @@ const calcNextPoint = () => {
     nextPoint.x = Math.abs((currentPoint.x + point2.x)) / 2;
     nextPoint.y = Math.abs((currentPoint.y + point2.y)) / 2;
     
-    $("body").append(
+    $("#triangle-box").append(
         $('<div></div>')
             .css('position', 'absolute')
             .css('top', nextPoint.y + 'px')
@@ -87,6 +87,8 @@ const calcNextPoint = () => {
     }
 };
 
+// $('#stop-btn').click(stopCount());
+
 // const myInterval = setInterval(calcNextPoint(), 10);
 
 const init = () => {
@@ -99,8 +101,8 @@ const stopCount = () => {
 }
 
 // Click to choose starting point
-$(document).ready(function () {
-    $(document).click(function (ev) {
+$('#triangle-box').ready(function () {
+    $('#triangle-box').click(function (ev) {
         mouseX = ev.pageX;
         mouseY = ev.pageY
         $("body").append(
