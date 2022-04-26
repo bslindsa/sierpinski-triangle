e4cfa25b01ce870e82b83ba22d238efe680f5b1e
Brad Lindsay <bslindsa@gmail.com>
2022-04-26 11:24:02 -0700

    Add Point max and color input fields. Color selection functional

--- script.js
+++ script.js
@@ -46,28 +46,25 @@ const calcNextPoint = () => {
     // Pick a random point on the triangle
     let vertex = Math.floor(Math.random() * 3);
     let size = Math.floor(Math.random() * 3);
-    let colorNum = Math.floor(Math.random() * 2);
-    let color = 'gold';
-    if (colorNum === 0) {
-        color = 'white';
-    } 
-    // else if (colorNum === 1) {
-    //     color = 'blue';
-    // }
-    
+    let colorNum = Math.floor(Math.random() * 3);
+    let color = $('#color1').val();
+    if ($('#color2').val() !=='#000000' && colorNum === 0) {
+        color = $('#color2').val();
+    }
+    else if ($('#color3').val() !=='#000000' && colorNum === 1) {
+        color = $('#color3').val();
+    }
+
     let point2 = topVert;
     if (vertex === 0) {
-        point2 = topVert;
-    } 
+        point2 = botRight;
+    }
     else if (vertex === 1) {
         point2 = botLeft;
-    } 
-    else {
-        point2 = botRight;
-    };
+    }
 
     nextPoint.x = Math.abs((currentPoint.x + point2.x)) / 2;
-    nextPoint.y = Math.abs((currentPoint.y + point2.y)) / 2; 
+    nextPoint.y = Math.abs((currentPoint.y + point2.y)) / 2;
 
     $("#triangle-box").append(
         $('<div class="dots"></div>')
@@ -77,54 +74,60 @@ const calcNextPoint = () => {
             .css('width', size + 1 + 'px')
             .css('height', size + 1 + 'px')
             .css('background-color', color));
-    
+
     currentPoint = nextPoint;
 
     points++;
     $('#points').text(points);
-   
-    if (points === 10) {
-        console.log('Stop!')
-    }
 };
 
 // Click to choose starting point
-$('#triangle-box').ready(function () {
-    $('#triangle-box').click(function (ev) {
-        mouseX = ev.pageX;
-        mouseY = ev.pageY
-        $("body").append(
-            $('<div id="start-point"></div>')
-                .css('position', 'absolute')
-                .css('top', mouseY + 'px')
-                .css('left', mouseX + 'px')
-                .css('width', '5px')
-                .css('height', '5px')
-                .css('background-color', 'white'));
-        currentPoint = {
-            x: mouseX,
-            y: mouseY
-        }
-        
-        let speed = $('#speed-input').val();
+$('#triangle-box').click(function (ev) {
+    mouseX = ev.pageX;
+    mouseY = ev.pageY;
+    currentPoint = {
+        x: mouseX,
+        y: mouseY
+    }
+    $("body").append(
+        $('<div class="start-point"></div>')
+            .css('position', 'absolute')
+            .css('top', mouseY + 'px')
+            .css('left', mouseX + 'px')
+            .css('width', '5px')
+            .css('height', '5px')
+            .css('background-color', 'white'));
 
-        const myInterval = setInterval(calcNextPoint, speed);
 
-        const stopCount = () => {
-            clearInterval(myInterval);
-            console.log('Stop!')
-        }
-        
-        $('#stop-btn').click(stopCount);
-    });
+    let speed = $('#speed-input').val();
+    // console.log(speed);
+
+    const myInterval = setInterval(calcNextPoint, speed);
+
+    const stopCount = () => {
+        clearInterval(myInterval);
+    }
+
+    $('#stop-btn').click(stopCount);
+    $('#clear-btn').click(stopCount);
 });
 
 // Clear dots
 const clearDots = () => {
-    $('.dots').remove();
-    $('#start-point').remove();
     points = 0;
     $('#points').text(points);
+    $('.start-point').remove();
+    $('.dots').remove();
 }
 
 $('#clear-btn').click(clearDots);
+$('#clear-color1').click(() => {
+    $('#color1').val('#ffffff');
+})
+$('#clear-color2').click(() => {
+    $('#color2').removeAttr('value');
+    console.log($('#color2').val())
+})
+$('#clear-color3').click(() => {
+    $('#color3').removeAttr('value');
+})
